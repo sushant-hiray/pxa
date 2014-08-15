f153f44126bdf101c16e3de0026f72e717e1a8aa
Nilesh <nileshsatishkulkarni@gmail.com>
2014-08-15 11:34:09 +0530

    verified code level

--- code/pga/geodesic_mean.m
+++ code/pga/geodesic_mean.m
@@ -1,32 +1,38 @@
-function [ mean ] = geodesic_mean(Samples,N,step_size)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-% Samples are normalized
-% N is number of Samples
-if (N ==0)
-    mean = 0
-    disp('No of Samples cannot be zero')
-    return
+function [ mean ] = geodesic_mean(S,N,step_size,threshold)
+    %UNTITLED2 Summary of this function goes here
+    %   Detailed explanation goes here
+    % Samples are normalized
+    % N is number of Samples
+    if (N ==0)
+        mean = 0
+        disp('No of Samples cannot be zero')
+        return
+    end
+    delta = 0.001;
+    mean = S(1,:) -delta;
+    mean  = mean/norm(mean)
+    prevmean = S(1,:);
+    meanChange = 1;
+    data_dim = size(mean);
+    if(threshold > 1)
+        disp('[geodesic_mean]Threshold should be less than 1')
+    end
+    iter =0;
+    while(norm(meanChange) > threshold)
+        sums=zeros(1,data_dim(1));
+        iter=iter+1;
+        for i =1:N
+            sums = sums+log_map(mean,S(i,:));
+        end
+        %sums
+        %size(sums)
+        gradF = (-1/N)*sums;
+        mean = exp_map(mean,(step_size*(-gradF)));
+        disp (mean);
+        meanChange = log_map(prevmean,mean)/(2*pi);
+        prevmean = mean;
+        threshold;
+    end
     
-    
-mean = Samples(1,:);
-prevmean = Samples(1,:);
-meanError;
-data_dim = 
-
-while(true)
-    
-sums=zeros(
-for i in 1:N
-    sums = sums+log_map(mean,S(i));
-    
-gradF = (-1/N)*sums;
-
-mean = exp_map(mean,(step_size*(-gradF));
-meanError = log_map(prevmean,mean);
-
-if meanError < 
-
-    
-end
-
+    disp('iterations')
+    disp(iter);
