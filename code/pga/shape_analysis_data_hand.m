4d33e5cbb8abb587643df360472ab98d8c34a46c
sushant-hiray <hiraysushant@gmail.com>
2014-08-18 22:09:42 +0530

    Code Cleanup

--- code/pga/shape_analysis_data_hand.m
+++ code/pga/shape_analysis_data_hand.m
@@ -14,12 +14,7 @@ function shape_analysis_data_hand(shapes)
     I = zeros(56:2:no_of_samples);
     I(:,1,:) = X;
     I(:,2,:) = Y;
-    hold on
-    %{
-    for i = 1:no_of_samples,
-       plot(I(:,1,i),I(:,2,i),'r+');
-    end
-    %}
+    
     
     meanShape = zeros(n,2);
     Scaled =I;
@@ -35,23 +30,24 @@ function shape_analysis_data_hand(shapes)
         scaled_pcaInput(i,:) = scaled_pcaInput(i,:)/norm_scaled;
     end
    
+    
+    figure()
+    hold on
+    
+    for i = 1:no_of_samples,
+       plot(I(:,1,i),I(:,2,i),'g+');
+    end
+    
+    plot(meanShape(1:n),meanShape(n+1:end),'r*-');
+    axis equal tight;
+    
+    
     %compute the variance
     [meanShape,V,D] = pga(scaled_pcaInput);
-    V;
-    D;
     disp('V and D sizes');
     size(V)
-    size(D)
-    diag(D);
-    %plot(diag(D),'ro-')
-    %{
-    for i = 1:no_of_samples,
-      plot(scaled_pcaInput(i,1:56),scaled_pcaInput(i,57:end),'r+');
-    end
-    %}
-    
-    
-    
+    size(D)    
+   
     [eigenValue1,row] = max(diag(D));
     size(V)
     eigenVect1 = V(:,row);
@@ -76,68 +72,46 @@ function shape_analysis_data_hand(shapes)
     norm (meanShape)
     norm (meanShape2)
    
-    %figure();
+    figure();
     hold on;
-    
-    size(meanShape3);
     plot(meanShape2(1:n),meanShape2(n+1:end),'g*-');
     plot(meanShape3(1:n),meanShape3(n+1:end),'m*-');
-    
     plot(meanShape(1:n),meanShape(n+1:end),'b*-');
     axis equal tight;
     
-    %[row,col] = find(D == max(abs(D(:))));
     
-    %{
+    [row,col] = find(D == max(abs(D(:))));
+    
+    
     eigenVect2 = V(:,row-1);
     eigenVect3 = V(:,row-2);
-    eigenVect1 = eigenVect1;
-    disp('diagonal D');
-    %plot(diag(D),'ro-')
-    [x,y] = size(D)
-    temp = zeros(y,1);
-    temp = temp +1;
-    temp = D * temp;
-    %}
-    %{
-    tem = diag(D);
-    eigenVal2 = tem(row-1); 
-    eigenVal3 = tem(row-2); 
-    %disp('column number is : ' + row);
-    %}
     
-  
-    %{
-    eigenDiff2 = zeros(n,2);
-    eigenDiff2(:,1) = eigenVect2(1:n);
-    eigenDiff2(:,2) = eigenVect2(n+1:end);
-    eigenDiff3 = zeros(n,2);
-    eigenDiff3(:,1) = eigenVect3(1:n);
-    eigenDiff3(:,2) = eigenVect3(n+1:end);
-    %}
-   
-    %{
-    meanShape4 = meanShape - 2*sqrt(eigenVal2)*eigenDiff2;
-    meanShape5 = meanShape + 2*sqrt(eigenVal2)*eigenDiff2;
-    meanShape6 = meanShape - 2*sqrt(eigenVal3)*eigenDiff3;
-    meanShape7 = meanShape + 2*sqrt(eigenVal3)*eigenDiff3;
-    %}
     
-   
-    %{
+    meanShape4 = - 2*sqrt(a)*eigenVect2';
+    meanShape5  = + 2*sqrt(a)*eigenVect2';
+    meanShape4 = exp_map(meanShape,meanShape4);
+    meanShape5 = exp_map(meanShape, meanShape5);
+    
+    
     figure();
-    
     hold on;
-    plot(meanShape4(:,1),meanShape2(:,2),'k*-');
-    plot(meanShape5(:,1),meanShape3(:,2),'r*-');
-    plot(meanShape(:,1),meanShape(:,2),'b*-');
+    plot(meanShape4(1:n),meanShape4(n+1:end),'g*-');
+    plot(meanShape5(1:n),meanShape5(n+1:end),'m*-');
+    plot(meanShape(1:n),meanShape(n+1:end),'b*-');
     axis equal tight;
+    
+    meanShape6 = - 2*sqrt(a)*eigenVect3';
+    meanShape7  = + 2*sqrt(a)*eigenVect3';
+    meanShape6 = exp_map(meanShape,meanShape6);
+    meanShape7 = exp_map(meanShape, meanShape7);
+    
     figure();
     hold on;
-    size(meanShape3);
-    plot(meanShape6(:,1),meanShape6(:,2),'g*-');
-    plot(meanShape7(:,1),meanShape7(:,2),'m*-');
-    plot(meanShape(:,1),meanShape(:,2),'b*-');
+    plot(meanShape6(1:n),meanShape6(n+1:end),'g*-');
+    plot(meanShape7(1:n),meanShape7(n+1:end),'m*-');
+    plot(meanShape(1:n),meanShape(n+1:end),'b*-');
+    axis equal tight;
+    
     
     figure();
     hold on;
@@ -152,23 +126,7 @@ function shape_analysis_data_hand(shapes)
         end
     end
     subplot(3,3,9);
-    plot(meanShape(:,1),meanShape(:,2),'b*-');
-    
-    figure();
-    hold on;
-     plot(meanShape(:,1),meanShape(:,2),'b*-');
-    for i = 1:no_of_samples,
-       plot(Scaled(:,1,i),Scaled(:,2,i),'r*');
-    end
-    
-    axis equal;
-    
-    figure();
-    hold on;
-    plot(meanShape(:,1),meanShape(:,2),'b*-');
-    
-    %}
-    
+    plot(meanShape(1:n),meanShape(n+1:end),'b*-');
     
     
     
