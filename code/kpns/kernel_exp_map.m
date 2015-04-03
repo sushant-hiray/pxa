1ca284d3d2ab792f917c5faecefcbfa7407fe569
Nilesh <nileshsatishkulkarni@gmail.com>
2015-04-03 14:42:50 +0530

    exp correcected

--- code/kpns/kernel_exp_map.m
+++ code/kpns/kernel_exp_map.m
@@ -1,5 +1,4 @@
 function [Out] = kernel_exp_map(G,KData,v0)
-
     num_points = size(KData,2);
     num_feat  = size(KData,1);
     D = sqrt(diag(KData'*G*KData));
@@ -7,6 +6,7 @@ function [Out] = kernel_exp_map(G,KData,v0)
     D = D(ones(num_feat,1),:);
     sinD = sin(D);
     cosD = cos(D);
+    D = (D> 1E-10).*D + (D<1E-10)*1E-10;
     bRep = v0(:,ones(1, num_points));
     Out = cosD.*bRep+ KData.*(sinD./D);
 end
--- code/kpns/kernel_test_script.m
+++ code/kpns/kernel_test_script.m
@@ -1,12 +1,12 @@
 in_dim =3;
-Data = randvonMisesFisherm(in_dim,1000, 100, [0 0 1])
-% Data(:,1) = zeros(in_dim,1);
-% Data(end,1) = 1/sqrt(2);
-% Data(1,1) = 1/sqrt(2);
-% Data(:,2) = zeros(in_dim,1);
-% Data(2,2) = 1;
-% Data(:,3) = zeros(in_dim,1);
-% Data(1,3) = 1;
+Data = randvonMisesFisherm(in_dim,1000, 100, [0 0 1]);
+Data(:,1) = zeros(in_dim,1);
+Data(end,1) = 1/sqrt(2);
+Data(1,1) = 1/sqrt(2);
+Data(:,2) = zeros(in_dim,1);
+Data(2,2) = 1;
+Data(:,3) = zeros(in_dim,1);
+Data(1,3) = 1;
 
 G = generateGramMatrix(Data,'Linear');
 num_points = size(Data,2);
@@ -17,5 +17,12 @@ log_map_Data = kernel_log_map(G,KData,v);
 temp = log_map_Data;
 TData = Data*temp;
 %TData2 = log_map(Data(:,end),Data);
-e =sum((TData(:)-TData2(:)).^2);
-%exp_map_Data = kernel_exp_map(G,log_map_Data,v);
+%se =sum((TData(:)-TData2(:)).^2);
+
+exp_map_Data = kernel_exp_map(G,log_map_Data,v);
+
+e = sum((diag(exp_map_Data-eye(num_points))).^2);
+FinalData = Data*exp_map_Data;
+
+FinalData2 = exp_map(Data(:,end),TData);
+
