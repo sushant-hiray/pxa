f24da335c891bf9a77dfb1c82974e7094578cd83
sushant <hiraysushant@gmail.com>
2015-05-16 21:17:15 +0530

    Added linear kernel for kpns

--- code/kpns/dim_reduction_orl.m
+++ code/kpns/dim_reduction_orl.m
@@ -67,10 +67,11 @@ options.degree=5;
 R_kpca_5 = 100*eig_val/sum(eig_val);
 
 options.KernelType='Linear';
+[Mapping,BkGm,R_pns,Res] = kernel_pnsMain(Data,1,PGS,options);
 [ eigvec, eig_val ] = kpca_main(Data,options);
 R_linear = 100*eig_val/sum(eig_val);
 % X_mean = mean(Data,2);
 % X_shift = Data -X_mean(:,ones(1,size(Data,2)));
 % [eigvec eigv ] = svd(X_shift);
 
-Total_R = [R_kernel(1:4) R_kpca_gauss(1:4) R_kernel_np3(1:4) R_kernel_np5(1:4) R_kpca_np3(1:4) R_kpca_np5(1:4) R_kpca_3(1:4) R_kpca_5(1:4) R_linear(1:4)]
+Total_R = [R_kernel(1:10) R_kpca_gauss(1:10) R_kernel_np3(1:10) R_kernel_np5(1:10) R_kpca_np3(1:10) R_kpca_np5(1:10) R_kpca_3(1:10) R_kpca_5(1:10) R_linear(1:10) R_pns(1:10)]
