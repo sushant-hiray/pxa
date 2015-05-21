0d7ed42cda50f6531c9f311acae813ea0718f913
sushant <hiraysushant@gmail.com>
2015-05-21 12:49:05 +0530

    Added ILPD QDR

--- code/kpns/dim_reduction_liver.m
+++ code/kpns/dim_reduction_liver.m
@@ -1,10 +1,15 @@
 %% COmments : KPNS > KPCA Guassian kernel 3-4%
 
 
+clc
+clear all
+close all
+rng(0)
+
+
 load('../../data/liver.mat');
-rng(0);
 %transfusion = normc(transfusion');
 PGS =1;
 liver = liver';
-Total_R = dim_reduction_data(liver,'liver');
+Total_R = dim_reduction_data(liver,'ILPD (Indian Liver Patient Dataset)');
 Total_R
