c92b2f663ced092d22cbdf612ca9466398a96d51
sushant <hiraysushant@gmail.com>
2015-05-21 13:33:37 +0530

    ADDED USPS QDR

--- code/kpns/dim_reduction_usps.m
+++ code/kpns/dim_reduction_usps.m
@@ -22,46 +22,8 @@ Data = double(Data);
 Data = Data - repmat(mean(Data), size(Data,1), 1);
 Data = normc(Data);
 
-    Data = [Data data(:,:,i)];
 
 PGS =1;
 
-
-options.KernelType='Gaussian';
-%[Mapping,BkGm,R_kernel,Res] = kernel_pnsMain(Data,1,PGS,options);
-[ eigvec, eig_val ] = kpca_main(Data,options);
-R_kpca_gauss = 100*eig_val/sum(eig_val);
-
-
-options.KernelType='NPolynomial';
-options.degree=10;
-%[Mapping,BkGm,R_kernel_np3,Res] = kernel_pnsMain(Data,1,PGS,options);
-[ eigvec, eig_val ] = kpca_main(Data,options);
-R_kpca_np3 = 100*eig_val/sum(eig_val);
-
-
-options.KernelType='NPolynomial';
-options.degree=5;
-%[Mapping,BkGm,R_kernel_np5,Res] = kernel_pnsMain(Data,1,PGS,options);
-[ eigvec, eig_val ] = kpca_main(Data,options);
-R_kpca_np5 = 100*eig_val/sum(eig_val);
-
-options.KernelType='Polynomial';
-options.degree=3;
-[ eigvec, eig_val ] = kpca_main(Data,options);
-R_kpca_3 = 100*eig_val/sum(eig_val);
-
-options.KernelType='Polynomial';
-options.degree=5;
-[ eigvec, eig_val ] = kpca_main(Data,options);
-R_kpca_5 = 100*eig_val/sum(eig_val);
-
-options.KernelType='Linear';
-%[Mapping,BkGm,R_pns,Res] = kernel_pnsMain(Data,1,PGS,options);
-[ eigvec, eig_val ] = kpca_main(Data,options);
-R_linear = 100*eig_val/sum(eig_val);
-% X_mean = mean(Data,2);
-% X_shift = Data -X_mean(:,ones(1,size(Data,2)));
-% [eigvec eigv ] = svd(X_shift);
-
-%Total_R = [R_kernel(1:10) R_kpca_gauss(1:10) R_kernel_np3(1:10) R_kernel_np5(1:10) R_kpca_np3(1:10) R_kpca_np5(1:10) R_kpca_3(1:10) R_kpca_5(1:10) R_linear(1:10) R_pns(1:10)]
+Total_R = dim_reduction_data(Data,'USPS Dataset');
+Total_R
