b304e8247a3f18dcabe53e6a4fa535c8e5563861
sushant <hiraysushant@gmail.com>
2015-05-21 20:07:26 +0530

    Added MNSI QDR Results

---
+++ code/kpns/dim_reduction_mnist_1.m
@@ -0,0 +1,22 @@
+%% COmments : KPNS > KPCA Guassian kernel 3-4%
+
+clc
+clear all
+close all
+rng(0)
+
+load('../../data/mnist_all.mat');
+
+Data = test1(1:300,:);
+Data = double(Data);
+Data = Data';
+
+% preprocessing
+Data = Data - repmat(mean(Data), size(Data,1), 1);
+Data = normc(Data);
+
+
+PGS =1;
+
+Total_R = dim_reduction_data(Data,'MNIST Class 1 Dataset');
+Total_R
